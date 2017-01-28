882e64d18cfc88824259dd594008c875b36a0a17
Yoshimxxx <b1402915@planet.kanazawa-it.ac.jp>
2017-01-28 13:03:51 +0900

    titlescene追加。なお処理((

---
+++ VR_Contents/Assets/Scripts/TitleSceneSCｓ/TitleCameraSC.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleCameraSC : MonoBehaviour {
+
+    public Camera cameraSC;
+    int raytime = 0;
+
+    // Use this for initialization
+    void Start () {
+        cameraSC = this.GetComponent<Camera>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        Ray ray = cameraSC.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.name == "start_sprite")//当たった物体
+            {
+                hit.transform.Rotate(0, 0, 5);
+                raytime++;
+                if (raytime > 100)//一定時間以上視点を合わせると
+                {
+                    
+                }
+            }
+            else
+            {
+                raytime = 0;
+            }
+
+        }
+    }
+}
